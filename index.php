3735cccad675557e0da2abed61d8da8ab898f4bf
LIPEDEV2025 <lizandrotechsite@gmail.com>
2025-07-12 18:25:08 -0500

    Primer comit subida del proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+echo "NUESTRO EQUIPO";
+echo "----------------";
+echo "Lizandro Lipe";
+echo "carrera: Computacion";
+echo "ciclo: v";
+echo "----------------";
+echo "Lilian Calderon";
+echo "carrera: Computacion";
+echo "ciclo: v";
+echo "-----------------";
+?>
